39abddde87eafd359a09dd94be1a8b2ef0937b7b
Riki aldi pari <rikialdipari@gmail.com>
2022-11-19 08:53:52 +0700

    pertemuan 5 dan 6

---
+++ src/algoritma/pertemuan5_array/Array1.java
@@ -0,0 +1,17 @@
+package algoritma.pertemuan5_array;
+
+public class Array1 {
+	public static void main(String[] args) {
+		String[] arrayStr = new String[3];
+		arrayStr[0] = "400";
+		arrayStr[1] = "500";
+		arrayStr[2] = "600";
+	
+		for (int i = 0; i < arrayStr.length; i++) {
+			System.out.println(arrayStr[i]);
+			System.out.println(i);
+		}
+		 
+	}
+
+}
---
+++ src/algoritma/pertemuan5_array/ArrayCopy.java
@@ -0,0 +1,21 @@
+package algoritma.pertemuan5_array;
+
+public class ArrayCopy {
+	public static void main(String[] args) {
+		String[] arrayStr = new String[3];
+		arrayStr[0] = "400";
+		arrayStr[1] = "500";
+		arrayStr[2] = "600";
+		
+		String[] arrayStrBaru = new String[3];
+		
+		arrayStrBaru[0]=arrayStr[0];	
+		arrayStrBaru[1]=arrayStr[1];
+		arrayStrBaru[2]=arrayStr[2];
+	
+		for (int i = 0; i < arrayStrBaru.length; i++) {
+			System.out.println(arrayStrBaru[i]);
+			System.out.println(i);
+		}
+	}
+}
